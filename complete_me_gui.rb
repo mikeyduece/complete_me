f5e5d9f08ae8df977dcc1c8f78e1425fbeb9ec01
Boots2291 <buteramv@gmail.com>
2017-07-12 19:12:46 -0600

    Refactor GUI

--- complete_me_gui.rb
+++ complete_me_gui.rb
@@ -5,28 +5,29 @@ dictionary = File.read("/usr/share/dict/words")
 cm.populate(dictionary)
 
 Shoes.app(title: "CompleteMe", width: 550) do
-  flow(margin: [15, 15, 0, 5]) do
+  flow(margin: [15, 15, 15, 5]) do
     para("Enter the start of a word to see suggestions.")
   end
-  flow(margin: [15, 10, 0, 0]) do
+  flow(margin: [15, 0, 15, 15]) do
     @prefix = edit_line
-    button "Suggest" do
-      @results.clear if @results
-      @suggestions = cm.suggest(@prefix.text)
-      @results = flow(margin: [15, 15, 0, 0]) do
-        @suggestions.each do |word|
-          para(
-            link(word, stroke: "black").click do
-              cm.select(@prefix.text, word)
-            end
-          )
-        end
-      end
-    end
+    @suggest_button = button "Suggest"
 
     button "Clear" do
       @results.clear
     end
   end
-end
 
+  @suggest_button.click do
+    @results.clear if @results
+    @suggestions = cm.suggest(@prefix.text)
+    @results = flow(margin: [15, 0, 15, 0]) do
+      @suggestions.each do |word|
+        para(
+          link(word, stroke: "black").click do
+            cm.select(@prefix.text, word)
+          end
+        )
+      end
+    end
+  end
+end
